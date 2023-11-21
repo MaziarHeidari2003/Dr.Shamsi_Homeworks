7fc8a4bb127859d07cdfe5bd40c1b0dd3863d74a
Maziar <144916498+MaziarHeidari2003@users.noreply.github.com>
2023-11-21 11:34:09 -0800

    Update problem5.py

--- python_2_problems/problem5.py
+++ python_2_problems/problem5.py
@@ -3,6 +3,8 @@ numXs = int(input("How many times should I print the letter X: "))
 toPrint = " "
 while numXs !=0 :
     toPrint =  "X" + toPrint
+    #see i think if the user input is 5 the output based on the question should be X X X X X ,
+    but here the output is XXXXX  and i dont know what to do about the damn space! 
     numXs -=1
 print(toPrint)    
 
