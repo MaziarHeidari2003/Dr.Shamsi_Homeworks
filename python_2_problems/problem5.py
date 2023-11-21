61181840685c96c38d94d3de97fce614b5f8c751
Maziar <144916498+MaziarHeidari2003@users.noreply.github.com>
2023-11-21 11:34:27 -0800

    Update problem5.py

--- python_2_problems/problem5.py
+++ python_2_problems/problem5.py
@@ -4,7 +4,7 @@ toPrint = " "
 while numXs !=0 :
     toPrint =  "X" + toPrint
     #see i think if the user input is 5 the output based on the question should be X X X X X ,
-    but here the output is XXXXX  and i dont know what to do about the damn space! 
+    #but here the output is XXXXX  and i dont know what to do about the damn space! 
     numXs -=1
 print(toPrint)    
 
